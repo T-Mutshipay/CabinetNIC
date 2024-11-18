97fec718ed1ec13b47158d8d4bf9eab5347028c7
T-Mutshipay <heritiermutshipay@gmail.com>
2024-11-18 18:14:55 +0100

    titre site

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title', 'Votre Titre Par Défaut')</title>
+        <title>@yield('title', 'N.I.C & Associés')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <link rel="preconnect" href="https://fonts.bunny.net">
