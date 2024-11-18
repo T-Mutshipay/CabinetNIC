b5afd6145f64dcadfc86dcc7bbf5184b7c1f402e
T-Mutshipay <heritiermutshipay@gmail.com>
2024-11-18 18:07:27 +0100

    migrations

---
+++ database/migrations/2024_11_16_153341_create_commentaires_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commentaires', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('actualite_id')->nullable(); 
+            $table->unsignedBigInteger('user_id')->nullable(); 
+            $table->text('contenu'); 
+            $table->timestamps();
+
+            $table->foreign('actualite_id')->references('id')->on('actualites')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commentaires');
+    }
+};
