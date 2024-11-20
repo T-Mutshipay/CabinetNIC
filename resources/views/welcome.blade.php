c035546cd9cc4e7385cd128632ec236800737ad7
T-Mutshipay <heritiermutshipay@gmail.com>
2024-11-20 13:12:24 +0100

    responisve et gestion de paragraphe des chaque section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
         <div class="container mx-auto px-4">
             <h2 class="text-4xl md:text-6xl font-bold mb-4 text-gold text-center">Nos services</h2>
             <div class="border-t border-gold my-4"></div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 md:gap-8">
                 <!-- Service 1: Consultations et Conseils Juridiques -->
-                <div class="flex flex-col items-center">
+                <div class="flex flex-col items-center m-1">
                     <div class="rounded-lg flex items-center justify-center h-48 w-48 sm:h-60 sm:w-60 bg-gold">
                         <svg class="w-16 h-16 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z" clip-rule="evenodd"/>
@@ -20,9 +20,9 @@
                         </p>
                     </div>
                 </div>
-    
+            
                 <!-- Service 2: Assistance et Représentation Extrajudiciaire -->
-                <div class="flex flex-col items-center">
+                <div class="flex flex-col items-center m-1">
                     <div class="rounded-lg flex items-center justify-center h-48 w-48 sm:h-60 sm:w-60 bg-gold">
                         <svg class="w-16 h-16 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z" clip-rule="evenodd"/>
@@ -35,9 +35,9 @@
                         </p>
                     </div>
                 </div>
-    
+            
                 <!-- Service 3: Assistance et Représentation Judiciaire -->
-                <div class="flex flex-col items-center">
+                <div class="flex flex-col items-centerm-1">
                     <div class="rounded-lg flex items-center justify-center h-48 w-48 sm:h-60 sm:w-60 bg-gold">
                         <svg class="w-16 h-16 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z" clip-rule="evenodd"/>
@@ -51,28 +51,30 @@
                     </div>
                 </div>
             </div>
+            
         </div>
     </div>
+    
     @endsection
     @section('team')  
-    <section class="m-14">
-        <div class="p-8 bg-gray-100 rounded-md">
-            <div class="container mx-auto text-center">
-                <h2 class="text-3xl md:text-5xl font-bold text-gold mb-4">Rencontrez Notre Équipe d'Avocats</h2>
-                <p class="text-lg md:text-xl text-gray-800">
-                    Chez N.I.C & Associés, notre équipe d'avocats est composée de professionnels hautement qualifiés et passionnés, dévoués à défendre vos droits et à vous offrir les meilleurs conseils juridiques. Chacun de nos avocats possède une expertise approfondie dans divers domaines du droit, vous assurant une assistance juridique complète et adaptée à vos besoins spécifiques.
-                </p>
-                <p class="text-lg md:text-xl text-gray-800 mt-4">
-                    Notre équipe se distingue par son approche collaborative et personnalisée. Nous croyons fermement que chaque client est unique, et nous nous efforçons de comprendre vos besoins et vos objectifs pour vous fournir des solutions juridiques sur mesure. Que vous soyez confronté à des questions complexes de droit commercial, à des litiges familiaux, ou à des défis de conformité réglementaire, notre équipe est prête à vous assister avec professionnalisme et compassion.
-                </p>
-                <p class="text-lg md:text-xl text-gray-800 mt-4">
-                    Notre engagement envers l'excellence se reflète dans nos valeurs fondamentales : intégrité, transparence et dévouement. Nous sommes fiers de notre réputation de cabinet d'avocats de confiance, reconnu pour notre expertise et notre capacité à obtenir des résultats favorables pour nos clients. En travaillant avec nous, vous bénéficiez non seulement de nos connaissances juridiques approfondies, mais aussi de notre réseau de ressources et de notre engagement indéfectible à protéger vos intérêts.
-                </p>
-                <p class="text-lg md:text-xl text-gray-800 mt-4">
-                    Explorez notre site pour en savoir plus sur chacun de nos avocats et découvrez comment notre équipe peut vous aider à naviguer dans le paysage juridique avec confiance et assurance. Bienvenue chez N.I.C & Associés, où votre réussite est notre priorité.
-                </p>
-            </div>
+    <div class="p-8 bg-gray-100 rounded-md">
+        <div class="container mx-auto text-center">
+            <h2 class="text-3xl md:text-5xl font-bold text-gold mb-4">Rencontrez Notre Équipe d'Avocats</h2>
+            <p class="text-lg md:text-xl text-gray-800 leading-relaxed text-justify">
+                Chez <span class="font-semibold">N.I.C & Associés</span>, notre équipe d'avocats est composée de professionnels hautement qualifiés et passionnés, dévoués à défendre vos droits et à vous offrir les meilleurs conseils juridiques. Chacun de nos avocats possède une expertise approfondie dans divers domaines du droit, vous assurant une assistance juridique complète et adaptée à vos besoins spécifiques.
+            </p>
+            <p class="text-lg md:text-xl text-gray-800 leading-relaxed text-justify">
+                Notre équipe se distingue par son approche collaborative et personnalisée. Nous croyons fermement que chaque client est unique, et nous nous efforçons de comprendre vos besoins et vos objectifs pour vous fournir des solutions juridiques sur mesure. Que vous soyez confronté à des questions complexes de droit commercial, à des litiges familiaux, ou à des défis de conformité réglementaire, notre équipe est prête à vous assister avec professionnalisme et compassion.
+            </p>
+            <p class="text-lg md:text-xl text-gray-800 leading-relaxed text-justify">
+                Notre engagement envers l'excellence se reflète dans nos valeurs fondamentales : <span class="font-semibold">intégrité, transparence et dévouement</span>. Nous sommes fiers de notre réputation de cabinet d'avocats de confiance, reconnu pour notre expertise et notre capacité à obtenir des résultats favorables pour nos clients. En travaillant avec nous, vous bénéficiez non seulement de nos connaissances juridiques approfondies, mais aussi de notre réseau de ressources et de notre engagement indéfectible à protéger vos intérêts.
+            </p>
+            <p class="text-lg md:text-xl text-gray-800 leading-relaxed text-justify">
+                Explorez notre site pour en savoir plus sur chacun de nos avocats et découvrez comment notre équipe peut vous aider à naviguer dans le paysage juridique avec confiance et assurance. Bienvenue chez <span class="font-semibold">N.I.C & Associés</span>, où votre réussite est notre priorité.
+            </p>
         </div>
+    </div>   
+    <section class="m-14">
         <div id="team" class="container mx-auto">
             <h2 class="text-4xl md:text-6xl font-bold mb-4 text-gold text-center">Notre équipe</h2>
             <div class="border-t border-gold  my-4"></div>
@@ -155,24 +157,25 @@
     </section>
     @endsection
     @section('articles')
-    <section class="m-4">
-        <div class="p-4 bg-gray-100 rounded-md m-4">
-            <div class="container mx-auto text-center">
-                <h2 class="text-3xl md:text-5xl font-bold text-gold mb-4">L'Importance de Rester Informé</h2>
-                <p class="text-lg md:text-xl text-gray-800">
-                    Dans un monde en constante évolution, rester informé des dernières actualités et des développements juridiques est essentiel. Chez N.I.C & Associés, nous comprenons l'importance de vous tenir à jour avec les informations qui peuvent impacter vos décisions et votre quotidien. C'est pourquoi nous avons créé cette section "Actualités et Articles".
-                </p>
-                <p class="text-lg md:text-xl text-gray-800 mt-4">
-                    Cette section est dédiée à vous fournir des analyses approfondies, des conseils pratiques et des mises à jour sur les changements législatifs. Nos experts partagent leurs connaissances et leurs perspectives pour vous aider à naviguer dans le paysage juridique complexe. Que vous soyez un professionnel du droit, un entrepreneur, ou un particulier, vous trouverez ici des ressources précieuses pour rester informé et prendre des décisions éclairées.
-                </p>
-                <p class="text-lg md:text-xl text-gray-800 mt-4">
-                    Parcourez nos articles pour découvrir des études de cas, des commentaires d'experts et des informations exclusives sur les tendances actuelles du droit. Nous sommes déterminés à vous fournir un contenu de qualité, pertinent et à jour. Votre connaissance est notre priorité, et nous sommes ici pour vous aider à chaque étape.
-                </p>
-                <p class="text-lg md:text-xl text-gray-800 mt-4">
-                    Bienvenue dans notre section "Actualités et Articles" – votre source d'informations juridiques fiables et instructives.
-                </p>
-            </div>
+    <div class="p-4 bg-gray-100 rounded-md m-4">
+        <div class="container mx-auto text-center">
+            <h2 class="text-3xl md:text-5xl font-bold text-gold mb-4">L'Importance de Rester Informé</h2>
+            <p class="text-lg md:text-xl text-gray-800 text-justify">
+                Dans un monde en constante évolution, rester informé des dernières actualités et des développements juridiques est essentiel. Chez <span class="font-semibold">N.I.C & Associés</span>, nous comprenons l'importance de vous tenir à jour avec les informations qui peuvent impacter vos décisions et votre quotidien. C'est pourquoi nous avons créé cette section "Actualités et Articles".
+            </p>
+            <p class="text-lg md:text-xl text-gray-800 mt-4 text-justify">
+                Cette section est dédiée à vous fournir des analyses approfondies, des conseils pratiques et des mises à jour sur les changements législatifs. Nos experts partagent leurs connaissances et leurs perspectives pour vous aider à naviguer dans le paysage juridique complexe. Que vous soyez un professionnel du droit, un entrepreneur, ou un particulier, vous trouverez ici des ressources précieuses pour rester informé et prendre des décisions éclairées.
+            </p>
+            <p class="text-lg md:text-xl text-gray-800 mt-4 text-justify">
+                Parcourez nos articles pour découvrir des études de cas, des commentaires d'experts et des informations exclusives sur les tendances actuelles du droit. Nous sommes déterminés à vous fournir un contenu de qualité, pertinent et à jour. Votre connaissance est notre priorité, et nous sommes ici pour vous aider à chaque étape.
+            </p>
+            <p class="text-lg md:text-xl text-gray-800 mt-4 text-justify">
+                Bienvenue dans notre section "Actualités et Articles" – votre source d'informations juridiques fiables et instructives.
+            </p>
         </div>
+    </div>
+    
+    <section class="m-4">
         
         <div class="container mx-auto mt-10">
             <h2 class="text-4xl md:text-6xl font-bold mb-4 text-gold text-center">Actualités et Articles</h2>
@@ -345,10 +348,10 @@
                     </div>
                 </div>
             </div>
-            <button id="prev" type="button" class="absolute top-1/2 -translate-y-1/2 left-0 z-30 p-2 rounded-full bg-gray-700 hover:bg-gray-800 text-white">
+            <button id="prev" type="button" class="absolute top-1/2 -translate-y-1/2 left-0 z-30 p-2 rounded-full bg-white hover:bg-gray-800 text-black font-bold">
                 <span>&larr;</span>
             </button>
-            <button id="next" type="button" class="absolute top-1/2 -translate-y-1/2 right-0 z-30 p-2 rounded-full bg-gray-700 hover:bg-gray-800 text-white">
+            <button id="next" type="button" class="absolute top-1/2 -translate-y-1/2 right-0 z-30 p-2 rounded-full bg-white hover:bg-gray-800 text-black">
                 <span>&rarr;</span>
             </button>
         </div>
