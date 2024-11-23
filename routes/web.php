54a924ce2e19831e6d643ebaed0ae4f50c50eefd
T-Mutshipay <heritiermutshipay@gmail.com>
2024-11-23 18:42:49 +0100

    route pour la création de commentaire

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActualiteController;
+use App\Http\Controllers\CommentaireController;
 
 Route::get('/', [ActualiteController::class, 'index'])->name('/');
 Route::get('/articles', function () {
@@ -10,5 +11,7 @@ Route::get('/articles', function () {
 Route::get('/avocats',function(){
     return view('avocats');
 })->name('avocats');
+
+Route::post('/',[CommentaireController::class,'store'])->name('comment.store');
 
 require __DIR__.'/web_actus.php';
