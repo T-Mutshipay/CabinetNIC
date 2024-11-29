29f49d4572528dc4d0be069da343dba99d6590cc
T-Mutshipay <heritiermutshipay@gmail.com>
2024-11-29 14:09:18 +0100

    ajout de la colonne role dans la table avocats

--- database/migrations/2024_11_16_151151_create_avocats_table.php
+++ database/migrations/2024_11_16_151151_create_avocats_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nom_complet');
             $table->string('email')->unique();
+            $table->string('role')->nullable();
             $table->text('description')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
