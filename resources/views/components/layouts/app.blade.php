0f69b3a1c7df9287a050354d9c974a39e9f2cf24
T-Mutshipay <heritiermutshipay@gmail.com>
2024-11-29 14:12:40 +0100

    ajustement de l'hamburger

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,41 +10,45 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-        <body class=" bg-slate-300">
+        <body class=" dark:bg-slate-300">
             <header class="relative bg-cover bg-center bg-no-repeat h-screen md:h-[80vh]" style="background-image: url('{{ asset('img/fond.jpg') }}');">
-                <nav class="border-b border-gray-200 bg-white  dark:bg-gray-900">
-                    <div class="max-w-screen-xl mx-auto flex items-center justify-between p-4">
+                <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                         <a href="{{ route('/') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-                            <img src="{{ asset('img/Cabinet.png') }}" class="h-16" alt="Logo" />
+                            <img src="{{ asset('img/Cabinet.png') }}" class=" h-14" alt="Logo" />
                             <span class="text-xl md:text-2xl font-semibold whitespace-nowrap text-gray-900 dark:text-white">
                                 N.I.C & Associés
                             </span>
                         </a>
-                        <button id="hamburger" class="md:hidden p-2 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-white" aria-controls="navbar-default" aria-expanded="false">
-                            <span class="sr-only">Ouvrir le menu</span>
-                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-                            </svg>
-                        </button>
-                        <ul id="navbar-default" class="hidden md:flex space-x-6 font-medium text-lg">
+                    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gold focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                        <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                             <li>
-                                <a href="{{ route('/') }}" class="text-gray-900 hover:text-gold dark:text-white dark:hover:text-yellow-500">
+                                <a href="{{ route('/') }}" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gold md:hover:bg-transparent md:border-0 md:hover:text-bg-gold dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                     Accueil
                                 </a>
                             </li>
+                            @if (request()->routeIs('/'))
                             <li>
-                                <a href="#services" class="text-gray-900 hover:text-gold dark:text-white dark:hover:text-yellow-500">
-                                    Nos services
+                                <a href="#services" 
+                                class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gold md:hover:bg-transparent md:border-0 md:hover:text-bg-gold dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">    Nos services
                                 </a>
                             </li>
+                            @endif
                             <li>
-                                <a href="#" class="text-gray-900 hover:text-gold dark:text-white dark:hover:text-yellow-500">
-                                    Honoraires
+                                <a href=" {{route('honoraires')}} " 
+                                class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gold md:hover:bg-transparent md:border-0 md:hover:text-bg-gold dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">    Honoraires
                                 </a>
                             </li>
                         </ul>
                     </div>
-                </nav>
+                </div>
+            </nav>
                 <script>
                     const hamburger = document.getElementById('hamburger');
                     const navbar = document.getElementById('navbar-default');
